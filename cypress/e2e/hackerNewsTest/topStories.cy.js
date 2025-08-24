1831aa4046f2ce5dcb3807ddfd4417366b7c9407
srujanatekale <srujanatekale@gmail.com>
2025-08-23 18:01:35 -0700

    fixed schema for topStories

--- cypress/e2e/hackerNewsTest/topStories.cy.js
+++ cypress/e2e/hackerNewsTest/topStories.cy.js
@@ -1,12 +1,12 @@
-import {testArrayHasValidNumbers} from '../../support/e2e.js'
 describe('topStories', () => {
-  it('gets json array with valid ids', () => {
-    cy.request('GET', 'https://hacker-news.firebaseio.com/v0/topstories.json').then((response) => {
+  beforeEach(() => {
+    cy.fixture('topStoriesSchema.json').as('schema');
+  });
+
+  it('gets json array with valid ids', function() {
+    cy.request('GET', 'https://hacker-news.firebaseio.com/v0/topstories.json').validateSchema(this.schema).then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body).to.be.an('array').and.have.length.above(0);
-        cy.wrap(testArrayHasValidNumbers(response.body)).then((result) => {
-            expect(result).to.be.true;
-        });
     })
   })
 })
